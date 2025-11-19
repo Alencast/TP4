16f6671d57c6b61db4667f6f84e01bd62013cfe8
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-11-19 09:03:53 -0300

    feat

---
+++ oficina/oficina/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for oficina project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from backend import views
+
+router = DefaultRouter()
+#exemplo de registro de uma viewset para o modelo Cliente
+router.register(r'clientes', views.ClienteViewSet)
+router.register(r'veiculos', views.VeiculoViewSet)
+router.register(r'pecas', views.PecaViewSet)
+router.register(r'orcamentos', views.OrcamentoViewSet)
+router.register(r'ordens-servico', views.OrdemServicoViewSet)
+router.register(r'itens-peca', views.ItemPecaViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
